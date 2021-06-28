d73b2ff2ec82d72218ca794151eb1aadd2933b66
Jeff Becker <jeff@i2p.rocks>
2021-06-28 06:52:01 -0400

    make it tarball

--- contrib/release.sh
+++ contrib/release.sh
@@ -7,9 +7,10 @@ mkdir -p $out
 cp lokinet-debian.iso $out/lokinet-debian-$built.iso
 
 echo "debian lokinet iso" > $out/readme.txt
-echo "packaged on $(date --date=@$build)" >> $out/readme.txt
+echo "packaged on $(date --date=@$built)" >> $out/readme.txt
 echo "users:" >> $out/readme.txt
 echo "guy:guy" >> $out/readme.txt
 echo "root:root" >> $out/readme.txt
 
-mktorrent -a https://opentracker.i2p.rocks/announce $out
+tar -xJvf $out $out.tar.xz
+mktorrent -a https://opentracker.i2p.rocks/announce $out.tar.xz
