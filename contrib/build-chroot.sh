032a54d1d8de58f165236de87dd6e7ab8b73c05e
Jeff <jeff@oxen.io>
2022-04-03 14:16:28 -0400

    redo web browser layer, dont use firefox-esr

--- contrib/build-chroot.sh
+++ contrib/build-chroot.sh
@@ -19,30 +19,51 @@ debootstrap \
 echo "lokinet-live" > $build/chroot/etc/hostname
 
 mkdir -p $build/chroot/etc/apt/trusted.gpg.d
-curl -so "$build/chroot/etc/apt/trusted.gpg.d/oxen.gpg" https://deb.oxen.io/pub.gpg
-
-mkdir -p "$build/chroot/etc/apt/sources.list.d"
-cat << 'EOF' > "$build/chroot/etc/apt/sources.list.d/oxen.list"
-deb http://deb.loki bullseye main
-EOF
+mkdir -p $build/chroot/etc/apt/sources.list.d
 
 cat << 'EOF' > "$build/chroot/install.sh"
 #!/usr/bin/env bash
+set -x
+set +e
 export DEBIAN_FRONTEND=noninteractive
-apt install -q -y ca-certificates
+apt update
+apt install -q -y ca-certificates apt-transport-https curl
+/install-repos.sh
+/install-debs.sh
 apt update && xargs apt install -q --no-install-recommends -y < /packages.txt
 adduser guy --quiet --disabled-password --gecos ""
 chpasswd <<<"root:root"
 chpasswd <<<"guy:guy"
-
+apt clean
 EOF
 
 echo "linux-image-$arch" > "$build/chroot/packages.txt"
 cat packages/*.txt >> "$build/chroot/packages.txt"
 
 mkdir -p "$build/chroot/tmp/debs"
-echo 'for f in /tmp/debs/*.deb ; do dpkg -i "$f" ; done' >> "$build/chroot/install.sh"
-echo 'rm -rf /tmp/debs' >> "$build/chroot/install.sh"
+echo '#!/bin/bash' > "$build/chroot/install-debs.sh"
+echo 'test $(find /tmp/debs/ | grep \\.deb$ -c ) != 0 || exit 0' >> "$build/chroot/install-debs.sh"
+echo 'for f in /tmp/debs/*.deb ; do dpkg -i "$f" ; done' >> "$build/chroot/install-debs.sh"
+echo 'rm -rf /tmp/debs' >> "$build/chroot/install-debs.sh"
+echo 'apt install -q -y -f' >> "$build/chroot/install-debs.sh"
+chmod +x "$build/chroot/install-debs.sh"
+
+echo '#!/bin/bash' > "$build/chroot/install-repos.sh"
+
+# deb.oxen.io repo
+
+echo 'curl -so /etc/apt/trusted.gpg.d/oxen.gpg https://deb.oxen.io/pub.gpg' >> "$build/chroot/install-repos.sh"
+echo 'cat << "EOF" > etc/apt/sources.list.d/oxen.list' >> "$build/chroot/install-repos.sh"
+echo 'deb https://deb.oxen.io bullseye main' >> "$build/chroot/install-repos.sh"
+echo 'EOF' >> "$build/chroot/install-repos.sh"
+
+# brave repo
+
+echo 'curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg' >> "$build/chroot/install-repos.sh"
+echo 'cat << "EOF" > /etc/apt/sources.list.d/brave.list' >> "$build/chroot/install-repos.sh"
+echo 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main' >> "$build/chroot/install-repos.sh"
+echo 'EOF' >> "$build/chroot/install-repos.sh"
+chmod +x "$build/chroot/install-repos.sh"
 
 for f in packages/*.deb ; do
     cp "$f" "$build/chroot/tmp/debs"
@@ -50,7 +71,7 @@ done
 
 chmod +x "$build/chroot/install.sh"
 chroot "$build/chroot" /install.sh
-rm -f "$build/chroot/install.sh" "$build/chroot/packages.txt"
+rm -f "$build/chroot/install.sh" "$build/chroot/install-debs.sh" "$build/chroot/install-repos.sh" "$build/chroot/packages.txt"
 
 mkdir -p $build/chroot/var/lib/lokinet/conf.d
 cp custom/lokinet/*.ini $build/chroot/var/lib/lokinet/conf.d
