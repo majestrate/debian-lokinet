2677e7bd52338fac2aeee688134d9fabed73a12a
Jeff <jeff@lokinet.io>
2022-06-29 10:05:49 -0400

    new tag

--- contrib/release.sh
+++ contrib/release.sh
@@ -10,7 +10,6 @@ echo "debian lokinet iso" > $out/readme.txt
 echo "packaged on $(date --date=@$built)" >> $out/readme.txt
 echo "users:" >> $out/readme.txt
 echo "guy:guy" >> $out/readme.txt
-echo "root:root" >> $out/readme.txt
 
 XZ_OPT='-T0' tar -cJvf $out.tar.xz $out
 mktorrent -a udp://open.stealth.si:80/announce -a https://opentracker.i2p.rocks/announce $out.tar.xz
